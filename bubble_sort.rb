84afc1d5052c5a030d9f0f8453870462b8d9b914
MsMauve <zoetropesilver@gmail.com>
2025-04-18 10:57:44 -0600

    finish bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,10 +13,24 @@ def element_comparison(array)
   array
 end
 
-# Tests for first iteration loop
-# So 1 loop through a big array
-def array_sorter(array)
-  array.each_with_index do |element, index|
-    
+# Tests for Wikipedia pseudocode implementation
+def bubble_sort(array)
+  length = array.length - 1
+  sorted = false
+  while sorted == false
+    sorted = true
+    (0...length).each do |i|
+      if array[i] > array[i + 1]
+        value1 = array[i]
+        value2 = array[i + 1]
+        array[i] = value2
+        array[i + 1] = value1
+        sorted = false
+      end
+    end
   end
+  array
 end
+
+test_array = [0, 4, 2, 6, 3, 7, 5, 9, 8, 1]
+puts bubble_sort(test_array)
