c9d607085b74ed0fd0fe6088e6cb216427ccbe74
MsMauve <zoetropesilver@gmail.com>
2025-04-18 10:11:55 -0600

    add basic element comparison

---
+++ bubble_sort.rb
@@ -0,0 +1,22 @@
+# def bubble_sort(array)
+  
+# end
+
+#element comparison test:
+def element_comparison(array)
+  if array[0] > array[1]
+    value1 = array[0]
+    value2 = array[1]
+    array[0] = value2
+    array[1] = value1
+  end
+  array
+end
+
+# Tests for first iteration loop
+# So 1 loop through a big array
+def array_sorter(array)
+  array.each_with_index do |element, index|
+    
+  end
+end
